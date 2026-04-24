ddd9f356a08afeda3550ade8dffce5262d3faa95
Cristian Bourceanu <v.c.bourceanu@sms.ed.ac.uk>
2026-04-24 23:48:28 +0100

    Update example usages of builtin filtering

--- src/bin/filter_target.rs
+++ src/bin/filter_target.rs
@@ -4,10 +4,9 @@ use aetherus_events::reader::CsvRecord;
 use aetherus_events::reader::read_csv;
 use aetherus_events::reader::read_ledger;
 
-use aetherus_events::pattern;
 use aetherus_events::SrcId;
-use aetherus_events::filter::{find_forward_uid_seq, BitsProperty};
-
+use aetherus_events::filter::{BitsProperty, find_forward_uid_seq};
+use aetherus_events::pattern;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -22,29 +21,68 @@ fn main() {
         Vec::new()
     };
 
+    //let filter_seq = vec![
+    //    pattern!(MCRT,      Interface, Refraction,                    SrcId::Surf(0xFFFF)),
+    //    pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::Mat(0xFFFF)),
+    //    pattern!(Detection,                                           SrcId::None),
+    //].into_iter().map(|bits_match| BitsProperty::Match(bits_match)).collect();
+    //
+    // Seawater scatter
+    //let filter_seq = vec![
+    //    pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::Mat(3)),
+    //    //pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::MatSurf(65535)),
+    //    //pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::MatSurf(65533)),
+    //    //pattern!(MCRT, Reflector, Specular, SrcId::Surf(0)),
+    //    pattern!(Detection,                                           SrcId::None),
+    //].into_iter().map(|bits_match| BitsProperty::Match(bits_match)).collect();
+
+    // Targets
     let filter_seq = vec![
-        pattern!(MCRT,      Interface, Refraction,                    SrcId::Surf(0xFFFF)),
-        pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::Mat(0xFFFF)),
-        pattern!(Detection,                                           SrcId::None),
-    ].into_iter().map(|bits_match| BitsProperty::Match(bits_match)).collect();
+        pattern!(Emission, SrcId::Light(0)),
+        pattern!(MCRT, Reflector, Specular, SrcId::Surf(0)), // TargetTube
+        //pattern!(MCRT, Reflector, Specular, SrcId::Surf(1)), // TargetToy
+        pattern!(Detection, SrcId::None),
+    ]
+    .into_iter()
+    .map(|bits_match| BitsProperty::Match(bits_match))
+    .collect();
+
+    // Glass specular
+    //let filter_seq = vec![
+    //    pattern!(MCRT, Interface, Reflection, SrcId::MatSurf(65534)), // External interface
+    //    //pattern!(MCRT, Interface, Reflection, SrcId::MatSurf(65535)), // Interface with water
+    //    pattern!(Detection,                                           SrcId::None),
+    //].into_iter().map(|bits_match| BitsProperty::Match(bits_match)).collect();
+
+    // Glass only
+    //let filter_seq = vec![
+    //    //BitsProperty::Match(pattern!(MCRT,      Interface, Refraction,                    SrcId::Surf(0xFFFF))),
+    //    BitsProperty::Match(pattern!(MCRT,      Material, Elastic, HenyeyGreenstein, Any, SrcId::Surf(0xFFFF))),
+    //    BitsProperty::NoMatch(pattern!(MCRT,      Reflector, Specular,                    SrcId::Surf(0))),
+    //    BitsProperty::NoMatch(pattern!(MCRT,      Reflector, Specular,                    SrcId::Surf(1))),
+    //    BitsProperty::Match(pattern!(Detection,                                           SrcId::None)),
+    //];
 
     println!("Filter seq: {:?}", filter_seq);
 
     let uids = find_forward_uid_seq(&ledger, filter_seq);
+    println!("Found {} matching UID sequences", uids.len());
     for uid in uids.clone() {
         println!("Found UID: {}", uid);
     }
 
-    let hex_uids = uids.iter()
-        .map(|uid| uid.encode())
-        .collect::<Vec<u64>>();
+    let hex_uids = uids.iter().map(|uid| uid.encode()).collect::<Vec<u64>>();
 
-    let phot_filtered = phot_records.iter()
-    .filter(|record| {
-        hex_uids.contains(&record.uid)
-    }).collect::<Vec<&CsvRecord>>();
+    let phot_filtered = phot_records
+        .iter()
+        .filter(|record| hex_uids.contains(&record.uid))
+        .collect::<Vec<&CsvRecord>>();
 
-    println!("Filtered photon records: len={} from {}", phot_filtered.len(), phot_records.len());
+    println!(
+        "Filtered photon records: len={} from {}",
+        phot_filtered.len(),
+        phot_records.len()
+    );
 
     let csv_dirpath = csv_path.map(|p| p.parent().unwrap().to_path_buf());
     let csv_outpath = if let Some(dirpath) = csv_dirpath {
@@ -53,10 +91,11 @@ fn main() {
         PathBuf::from("filtered_photons.csv")
     };
 
-    let mut csv_writer = csv::Writer::from_path(csv_outpath)
-        .expect("Unable to create output CSV file");
+    let mut csv_writer =
+        csv::Writer::from_path(csv_outpath).expect("Unable to create output CSV file");
     for filtered_record in phot_filtered {
-        csv_writer.serialize(&filtered_record)
-        .expect("Unable to write filtered CSV file");
+        csv_writer
+            .serialize(&filtered_record)
+            .expect("Unable to write filtered CSV file");
     }
 }
