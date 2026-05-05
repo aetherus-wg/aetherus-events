e5458731f05fb9433a285b5b8b8f72e9da56cfb8
Cristian Bourceanu <v.c.bourceanu@sms.ed.ac.uk>
2026-05-05 19:22:43 +0100

    Expose both default LedgerNode and LedgerTree to be used externall by the DSL

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,8 +4,14 @@ pub use crate::mcrt_event;
 pub use crate::src::SrcId;
 pub use crate::uid::Uid;
 
-use crate::ledger::LedgerTree as GenericLedgerTree;
+const MAP_SIZE: usize = 4;
+
+use crate::ledger::{
+    LedgerTree as GenericLedgerTree,
+    LedgerNode as GenericLedgerNode,
+};
 use crate::maps::SmallMap;
 
 /// Type alias for the LedgerTree that was found to be optimal to use for MCRT simulations.
-pub type LedgerTree = GenericLedgerTree<u32, SmallMap<u32, 4>>;
+pub type LedgerTree = GenericLedgerTree<u32, SmallMap<u32, MAP_SIZE>>;
+pub type LedgerNode = GenericLedgerNode<u32, SmallMap<u32, MAP_SIZE>>;
